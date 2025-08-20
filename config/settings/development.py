0e90f6eb41f6ea97f947da58fd1337b40da9f4fc
ProfHezzy <hezekiahonline94@gmail.com>
2025-08-20 15:49:16 +0100

    Updated requirements and project setup

--- config/settings/development.py
+++ config/settings/development.py
@@ -4,7 +4,8 @@ Development settings for AgroMarket project.
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
