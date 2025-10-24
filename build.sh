a59022573cc5e019679e20c732ccb702bd1abe04
ProfHezzy <hezekiahonline94@gmail.com>
2025-10-25 00:02:54 +0100

    feat: Increase collectstatic verbosity for debugging

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --verbosity 3
 python manage.py migrate
 
 # Create a directory for temporary media uploads
